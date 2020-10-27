563f351e451fc7174c06bfd269f389fe85c0303d
Guillermo Talavero beltran <gtalaver@c2r16s1.42madrid.com>
2020-10-27 17:34:07 +0100

    +autoindex nginx

--- srcs/mysql-conf.sql
+++ srcs/mysql-conf.sql
@@ -1,7 +1,7 @@
 --Generate DB 'wordpressdb';
 CREATE DATABASE wordpressdb DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
---Generate user 'wpuser' with 'wppass' password
+--Generate user 'wpuser' with its password
 CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'ml0978HJB';
 
 --Give permissions for wpuser to wordpressdb
